7c2f6f83678371ee80aa8e025451d955e7a24dd4
uchiyu <s17g453@stu.kagawa-u.ac.jp>
2017-08-18 12:18:34 +0900

    rspec

---
+++ spec/stack_rspec.rb
@@ -0,0 +1,15 @@
+#require './stack'
+require 'RSpec'
+
+RSpec.describe 'Stack' do
+  #subject { @stack }
+  #before do
+  #  @stack = Stack.new
+  #end
+  it { expect(true).to be true }
+  #describe '#is_empty' do
+  #  context '空の場合true' do
+  #    
+  #  end
+  #end
+end
